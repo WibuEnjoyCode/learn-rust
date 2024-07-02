6f005faa143d1d409cf734760df69929ee67a509
sagakyuu <wiliamferysagala@gmail.com>
2024-07-02 08:14:13 +0700

    first commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+#[test]
+fn hello_test() {
+    println!("Hello Test")
+}
+
+#[test]
+fn test_variable() {
+    let name: &str = "Wiliam Fery Sagala";
+    println!("Hello {}", name)
+}
